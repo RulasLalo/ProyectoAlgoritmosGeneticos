4a06ff1eaabdb945d7b0ef237fadbabf32d334f8
RulasLalo <raulcruez@gmail.com>
2021-02-05 14:31:09 -0600

    Nuevos cambios

--- Videojuego/Principal.py
+++ Videojuego/Principal.py
@@ -6,8 +6,7 @@ from Recorrido import Automata
 class Aplicacion:
     def __init__(self):
         auto=Automata()
-        print("-----------------------------------------------------------------------------------------------------------------------------------------------------------")
-        print("-----------------------------------------------------------------------------------------------------------------------------------------------------------")
+    
         print(f"Año 2065; el ser humano aprendió a cuidar sus recursos, los autos son {1:.02%} eléctricos, "
         "la deforestación se detuvo por completo hace 30 años; y el ser humano al fin pudo aprender a cuidar su hogar."
         "Pero ahora lo que está en la mira de todos es un contacto extraterrestre el cual se hizo hace 10 años con ondas de radio."
@@ -18,8 +17,7 @@ class Aplicacion:
         "mandados fuera de nuestro sistema solar, específicamente al planeta Kepler-22B. "
         "El descubrimiento de este mundo fue anunciado el día 5 de diciembre del 2011."
         "Tu objetivo es ayudara los ingenieros Airam y Alessandra a llegar a Kepler-22B en esta aventura interestelar.")
-        print("-----------------------------------------------------------------------------------------------------------------------------------------------------------")
-        print("-----------------------------------------------------------------------------------------------------------------------------------------------------------")
+        
         auto.escogerNave()
 
 app=Aplicacion()
